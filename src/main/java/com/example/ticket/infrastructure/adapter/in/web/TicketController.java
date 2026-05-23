52344ff3aa33cff3458496677f7cd9a276d6e67d
Dominique Cofre <do.cofre@duocuc.cl>
2026-05-23 10:06:00 -0400

    fix: agregar import Qualifier en TicketController

--- src/main/java/com/example/ticket/infrastructure/adapter/in/web/TicketController.java
+++ src/main/java/com/example/ticket/infrastructure/adapter/in/web/TicketController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.example.ticket.domain.model.EstadoTicket;
 import com.example.ticket.domain.model.Ticket;
